9a856e3108d78862a468dc75af2fc71082cd771d
zijue <460195657@qq.com>
2022-02-09 23:28:51 +0800

    feat: 添加工程列表

---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+//!!value 求value的布尔值
+export const isFalsy = (value) => (value === 0 ? true : !!value);
+
+//在一个函数里，改变传入的对象本身是不好的
+export const cleanObject = (object) => {
+  const result = Object.assign({}, object);
+  Object.keys(result).forEach((key) => {
+    const value = object[key];
+    if (!isFalsy(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
