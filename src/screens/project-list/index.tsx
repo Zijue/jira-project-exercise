5bf4231c8438bd4fb486192abcc385d09107f563
zijue <460195657@qq.com>
2022-02-14 11:37:44 +0800

    feat: 优化项目列表页

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,6 +4,7 @@ import { SearchPanel } from "./search-panel";
 import * as qs from "qs";
 import { cleanObject, useDebounce, useMount } from "utils";
 import { useHttp } from "utils/http";
+import styled from "@emotion/styled";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -24,9 +25,14 @@ export const ProjectListScreen = () => {
     client("users").then(setUsers);
   });
   return (
-    <div>
+    <Container>
+      <h2>项目列表</h2>
       <SearchPanel param={param} setParam={setParam} users={users} />
       <List list={list} users={users} />
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,4 +1,5 @@
-import { Input, Select } from "antd";
+/* @jsxImportSource @emotion/react */
+import { Form, Input, Select } from "antd";
 
 export interface User {
   id: number;
@@ -20,33 +21,39 @@ interface SearchPanelProps {
 
 export const SearchPanel = ({ param, setParam, users }: SearchPanelProps) => {
   return (
-    <form action="">
-      <Input
-        type="text"
-        value={param.name}
-        onChange={(event) =>
-          setParam({
-            ...param,
-            name: event.target.value,
-          })
-        }
-      />
-      <Select
-        value={param.personId}
-        onChange={(value) =>
-          setParam({
-            ...param,
-            personId: value,
-          })
-        }
-      >
-        <Select.Option value={""}>负责人</Select.Option>
-        {users.map((user) => (
-          <Select.Option key={user.id} value={user.id}>
-            {user.name}
-          </Select.Option>
-        ))}
-      </Select>
-    </form>
+    //css={/* 行内样式代码 */} emotion行内样式，需要在当前文件顶部添加 /* @jsxImportSource @emotion/react */
+    <Form css={{ marginBottom: "2rem" }} layout="inline">
+      <Form.Item>
+        <Input
+          placeholder="项目名"
+          type="text"
+          value={param.name}
+          onChange={(event) =>
+            setParam({
+              ...param,
+              name: event.target.value,
+            })
+          }
+        />
+      </Form.Item>
+      <Form.Item>
+        <Select
+          value={param.personId}
+          onChange={(value) =>
+            setParam({
+              ...param,
+              personId: value,
+            })
+          }
+        >
+          <Select.Option value={""}>负责人</Select.Option>
+          {users.map((user) => (
+            <Select.Option key={user.id} value={user.id}>
+              {user.name}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+    </Form>
   );
 };
